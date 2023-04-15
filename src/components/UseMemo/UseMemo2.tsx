2f1b50c5bc5f7d23b2073cfff56b5d0dfb0e6866
SenthilKumaranC <mailtocskumaran@gmail.com>
2023-04-15 08:45:08 +0530

    eventloop and form

--- src/components/UseMemo/UseMemo2.tsx
+++ src/components/UseMemo/UseMemo2.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const UseMemo2 = () => {
   const [gender, setGender] = useState("Female");
@@ -9,18 +9,56 @@ const UseMemo2 = () => {
   const fullName = useMemo(() => {
     if (gender === "Male") {
       return "Mr." + firstName + " " + lastName;
-    }else{
-        if(gender==="Female"){
-            if(maritalStatus==="Married"){
-                return "Mrs." + firstName + " " + lastName;
-            }else{
-                return "Miss." + firstName + " " + lastName;
-            }
+    } else {
+      if (gender === "Female") {
+        if (maritalStatus === "Married") {
+          return "Mrs." + firstName + " " + lastName;
+        } else {
+          return "Miss." + firstName + " " + lastName;
         }
+      }
     }
   }, [firstName, gender, lastName, maritalStatus]);
 
-  return <span>{fullName}</span>;
+  const listenForFirstNameChange = useCallback((dataFromInput:any)=>{
+    console.log(dataFromInput.target.value);
+    const input = dataFromInput.target.value.length;
+    setFirstName(("a").repeat(input));
+  },[])
+  const listenForSecondNameChange = useCallback((dataFromInput:any)=>{
+    console.log(dataFromInput.target.value);
+    setLastName(dataFromInput.target.value);
+  },[])
+  const listenForGenderChange = useCallback((dataFromInput:any)=>{
+    console.log(dataFromInput.target.value);
+    setGender(dataFromInput.target.value);
+  },[])
+  const listenForMartialstatusChange = useCallback((dataFromInput:any)=>{
+    console.log(dataFromInput.target.value);
+    setMaritalStaus(dataFromInput.target.value);
+  },[])
+
+  return (
+    <>
+      <form>
+        <input placeholder="Enter First Name" value={firstName} onChange={listenForFirstNameChange}></input>
+        
+        <input placeholder="Enter Last Name" value={lastName} onChange={listenForSecondNameChange}></input>
+        
+        <select value={gender} onChange={listenForGenderChange}>
+          <option value="Male">MALE</option>
+          <option value="Female">FEMALE</option>
+        </select>
+        
+        <select value={maritalStatus} onChange={listenForMartialstatusChange}>
+          <option value="Married">MARRIED</option>
+          <option value="Unmarried">UNMARRIED</option>
+        </select>
+        
+      </form>
+      <span>{fullName}</span>;
+    </>
+  );
 };
 
 export default UseMemo2;
